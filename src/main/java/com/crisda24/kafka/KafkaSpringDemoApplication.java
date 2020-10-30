e6ef46cf58625aae54c306269b920b488749bc4a
Cristian <crgaonas24@gmail.com>
2020-10-29 23:51:22 -0500

    Realizando callbacks de forma asíncrona mediante el producer

--- src/main/java/com/crisda24/kafka/KafkaSpringDemoApplication.java
+++ src/main/java/com/crisda24/kafka/KafkaSpringDemoApplication.java
@@ -1,5 +1,7 @@
 package com.crisda24.kafka;
 
+import java.awt.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,7 +9,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaProducerException;
+import org.springframework.kafka.core.KafkaSendCallback;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.util.concurrent.ListenableFuture;
 
 @SpringBootApplication
 public class KafkaSpringDemoApplication implements CommandLineRunner {
@@ -28,6 +34,27 @@ public class KafkaSpringDemoApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String...args) throws Exception {
-		kafkaTemplate.send("crisda24-topic","Sample message" );
+	
+		ListenableFuture<SendResult<String, String>> future =  kafkaTemplate.send("crisda24-topic","Sample message" );
+	    future.addCallback(new KafkaSendCallback<String, String>() {
+
+			@Override
+			public void onSuccess(SendResult<String, String> result) {
+				log.info("Message send ", result.getRecordMetadata().offset());
+			}
+
+			@Override
+			public void onFailure(Throwable ex) {
+				log.error("Error sending message ", ex);
+				
+			}
+
+			@Override
+			public void onFailure(KafkaProducerException ex) {
+				log.error("Error sending message ", ex);
+				
+			}
+		});
 	}
+	
 }
